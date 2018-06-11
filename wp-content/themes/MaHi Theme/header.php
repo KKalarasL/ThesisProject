08977f6858f36d6a04290575962a65025dfc49e5
Konstantinos Kalaras <konstantinos.kalaras@upstreamsystems.com>
2018-06-11 17:39:08 +0300

    Adding More Files(Need Changes)

--- wp-content/themes/MaHi Theme/header.php
+++ wp-content/themes/MaHi Theme/header.php
@@ -4,4 +4,28 @@
     <?php wp_head(); ?>
   </head>
   <body>
-    <h1>Mythology & History (MaHi)</h1>
+    <header class="site-header">
+    <div class="container">
+      <h1 class="school-logo-text float-left"><a href="#"><strong>Mythology & History</strong> MaHi</a></h1>
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <div class="site-header__menu group">
+        <nav class="main-navigation">
+          <ul class="min-list group">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Mythology</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#">MaHiGame</a></li>
+            <li><a href="#">Forum</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <div class="site-header__util">
+          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+          <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </div>
+      </div>
+    </div>
+  </header>
