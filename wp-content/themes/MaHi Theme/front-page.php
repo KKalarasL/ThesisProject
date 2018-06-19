ae7d09f80324f453eb2e1c6988708de543ab79e9
KKalarasL <konstantinos.kalaras@upstreamsystems.com>
2018-06-19 17:17:14 +0300

    Add MUPlugins, custom post types & some styling

--- wp-content/themes/MaHi Theme/front-page.php
+++ wp-content/themes/MaHi Theme/front-page.php
@@ -12,6 +12,55 @@
     </div>
   </div>
 
+  <div class="full-width-split group">
+
+    <div class="full-width-split__one">
+      <div class="full-width-split__inner">
+        <div class="image-box"><img src="<?php echo get_theme_file_uri('/images/History.jpg') ?>"></div>
+      </div>
+    </div>
+
+    <div class="full-width-split__two">
+      <div class="full-width-split__inner">
+        <div class="image-box"><img src="<?php echo get_theme_file_uri('/images/Mythology.jpg') ?>"></div>
+	    </div>
+	  </div>
+
+	</div>
+
+      <div class="about-us-image"><img src="<?php echo get_theme_file_uri('/images/about.jpg') ?>"><div class="about-us-content"><h1>~ About Us ~</h1><hr><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem.</p><hr></div></div>
+      
+
+	  <div class="hero-slider">
+	  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+	    <div class="hero-slider__interior container">
+	      <div class="hero-slider__overlay">
+	        <h2 class="headline headline--medium t-center">Free Transportation</h2>
+	        <p class="t-center">All students have free unlimited bus fare.</p>
+	        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+	    <div class="hero-slider__interior container">
+	      <div class="hero-slider__overlay">
+	        <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+	        <p class="t-center">Our dentistry program recommends eating apples.</p>
+	        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+	    <div class="hero-slider__interior container">
+	      <div class="hero-slider__overlay">
+	        <h2 class="headline headline--medium t-center">Free Food</h2>
+	        <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+	        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
   <?php get_footer();
 
 ?>
